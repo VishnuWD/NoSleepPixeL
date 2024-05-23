f474ecb640ef922df2b0a88aa803bcc9716b0990
VishnuWD <vishnuyadavhome@gmail.com>
2024-05-23 17:55:22 +0530

    change responsiveness of header section

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,8 +6,8 @@ import Header from "./Header";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "NS Pixel - Creative Web Design Team",
-  description: "NoSleepPixel: Unleash your website's potential with our creative collective. We specialize in eye-catching web design and responsive front-end solutions. Our team of experts is dedicated to creating user-friendly, aesthetically pleasing websites that cater to all your digital needs. Whether it's an e-commerce platform, a personal blog, or a business advertisement, we have the skills and experience to bring your vision to life. Our services include website design, development, maintenance, and support. We focus on delivering high-quality, mobile-friendly websites that ensure a seamless experience across all devices. Partner with NoSleepPixel for innovative web solutions and exceptional digital experiences.",
+  title: "NS Pixel - Creative Web Design & Development Solutions",
+  description: "NoSleepPixel: Unleash your website's potential with our creative collective. We specialize in eye-catching web design and responsive front-end solutions. Explore our services in e-commerce, personal websites, business ads, and more. Contact us today for a seamless digital experience.",
   keywords: "NoSleepPixel, web design, front-end solutions, responsive design, e-commerce, personal websites, business ads, digital experiences, creative collective, web development, user-friendly websites, digital services, website support, mobile-friendly websites, no sleep pixel",
   canonical: "https://www.nosleeppixel.com/",
   author: "NoSleepPixel Team",
