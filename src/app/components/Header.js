39932591cd63e16d7e49168f17f99ce9a951b0d1
VishnuWD <vishnuyadavhome@gmail.com>
2024-06-30 01:28:49 +0530

    made functional menu

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -1,48 +1,59 @@
 "use client"
-import React, { useEffect } from 'react'
+import Link from 'next/link';
+import React, { useEffect, useRef } from 'react'
 
 function Header() {
   // script.js
 
+
   useEffect(() => {
     let lastScrollTop = 0;
-const navbar = document.getElementById('nav');
+    const navbar = document.getElementById('nav');
 
-window.addEventListener('scroll', function() {
-    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    window.addEventListener('scroll', function () {
+      let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 
-    if (scrollTop > lastScrollTop) {
-        navbar.style.top = '-60px'; 
-    } else {
+      if (scrollTop > lastScrollTop) {
+        navbar.style.top = '-60px';
+      } else {
         navbar.style.top = '0';
-    }
-    lastScrollTop = scrollTop;
-});
-  
-  
+      }
+      lastScrollTop = scrollTop;
+    });
+
+
+
+
   }, [])
-  
+
 
 
   return (
     <>
       <nav id="nav">
         <input className='peer hidden' type="checkbox" name="" id="check" />
-        <label htmlFor="check" className=' z-30'>
+        <label htmlFor="check" className='z-30 cursor-pointer'>
           <div className='bg-white w-[5vmin] h-[2px] mb-1'></div>
           <div className='bg-white w-[7.7vmin] h-[2px] mb-1'></div>
           <div className='bg-white w-[11vmin] h-[2px] mb-1'></div>
         </label>
-        <div className='nav__list fixed blur-3xl bg-blend-normal hidden h-0 w-0 peer-checked:block left-[-20vw] top-[-10vh] peer-checked:animate-[navList_0.4s_forwards] z-20 bg-[#EDBFFF] rounded-[0_0_500px_30px]'>
-          <p>something</p>
+
+        <div className='fixed  hidden peer-checked:block left-[5vmin] text-[0vmin] top-[10vmin] select-none shadow-lg rounded-xl  peer-checked:animate-[navList_0.2s_forwards]'>
+          <div className='flex flex-col bg-[#0F0D0D]  gap-4  border rounded-xl p-[5vmin_7vmin] '>
+    
+            <Link className='hover:underline tracking-widest' href="/">Home</Link>
+            <Link className='hover:underline tracking-widest' href="about">About</Link>
+            <Link className='hover:underline tracking-widest' href="plans">Plans</Link>
+          </div>
         </div>
-        <a href="/"><h2 className='z-30'>NS Pixel<p>🌙</p></h2></a>
+
+        <Link href="/"><h2 className='z-30'>NS Pixel<p>🌙</p></h2></Link>
 
         <a className='z-30' href="/#FOOTER">
           <h4>Contact</h4>
         </a>
       </nav>
-      
+
     </>
   )
 }
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ export default function NotFound() {
     return (
        
   <main class="h-screen w-full flex flex-col justify-center items-center bg-[#0F0D0D]">
-    <Curser/>
+
       <h1 class="text-9xl font-extrabold text-white tracking-widest">404</h1>
       <div class="bg-[#EDBFFF] px-2 text-sm rounded text-[#0F0D0D] rotate-12 absolute border-[#0F0D0D] border-2">
           Page Not Found
